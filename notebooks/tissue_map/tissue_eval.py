f3387b132ee58249ce162535b8ea319facb140b1
Declan Lim <declan.lim@mail.utoronto.ca>
2023-05-31 13:38:29 +0000

    completed tissue_map_1.0 and eval file

--- notebooks/tissue_map/tissue_eval.py
+++ notebooks/tissue_map/tissue_eval.py
@@ -5,15 +5,25 @@ def attribute_graph(sample_dict):
     # sample_dict should have the format {sample_id: [attribute1, attribute2, ...]}
     attribute_count = {}
 
-    for key, value in sample_dict.items():
+    for value in sample_dict.values():
         num_attrs = len(value)
-    if num_attrs in attribute_count:
-        attribute_count[num_attrs] += 1
-    else:
-        attribute_count[num_attrs] = 1
+        if num_attrs in attribute_count:
+            attribute_count[num_attrs] += 1
+        else:
+            attribute_count[num_attrs] = 1
 
 
-    bar_data = {"num_attributes": list(attribute_count.keys()), "num_samples": list(attribute_count.values()), "label": list(attribute_count.values())}
-    fig = px.bar(bar_data, x="num_attributes", y="num_samples", text="label")
-    fig.update_layout(title="Number of attributes found per biosample", xaxis=dict(title="# of attributes assigned to biosample"), yaxis=dict(title="Count"))
+    bar_data = {'num_attributes': list(attribute_count.keys()), 'num_samples': list(attribute_count.values()), 'label': list(attribute_count.values())}
+    fig = px.bar(bar_data, x='num_attributes', y='num_samples', text='label')
+    fig.update_layout(title='Number of attributes found per biosample', xaxis=dict(title='# of attributes assigned to biosample'), yaxis=dict(title='Count'))
+    fig.show()
+
+def matches_graph(sample_dict):
+    # plots a graph of number of samples matched to BRENDA vs number of samples not matched to BRENDA
+    no_match_count = list(sample_dict.values()).count(None)
+    match_count = 10000 - no_match_count
+
+    bar_data = {'match': ['Match', 'No match'], 'count': [match_count, no_match_count], 'label': [match_count, no_match_count]}
+    fig = px.bar(bar_data, x='match', y='count', text='label')
+    fig.update_layout(title='Number of Samples Matched to Brenda', xaxis=dict(title='Match'), yaxis=dict(title='Count'))
     fig.show()
